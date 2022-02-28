8c88f4cf260800470fec17ba164b542328cab558
skotsuki <42108607+skotsuki@users.noreply.github.com>
2022-02-28 10:49:00 +0900

    Add files via upload

---
+++ speedy/letkf/CMakeFiles/letkf020.m01.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/SFMT.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/common.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/common_mpi.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/common_time.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/common_mtx.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/netlib2.f.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/common_letkf.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/common/common_lpf.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/speedy/common/common_speedy.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/speedy/common/common_obs_speedy.f90.o"
+  "CMakeFiles/letkf020.m01.dir/data02/oishi/speedy-tsubasa/speedy/common/common_mpi_speedy.f90.o"
+  "CMakeFiles/letkf020.m01.dir/letkf_obs.f90.o"
+  "CMakeFiles/letkf020.m01.dir/interpolate.f90.o"
+  "CMakeFiles/letkf020.m01.dir/letkf_tools.f90.o"
+  "CMakeFiles/letkf020.m01.dir/letkf.f90.o"
+  "letkf020.m01.pdb"
+  "letkf020.m01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/letkf020.m01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ speedy/letkf/CMakeFiles/letkf020.m01.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,54 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "common.mod"
+  "COMMON.mod"
+  "CMakeFiles/letkf020.m01.dir/common.mod.stamp"
+
+  "common_letkf.mod"
+  "COMMON_LETKF.mod"
+  "CMakeFiles/letkf020.m01.dir/common_letkf.mod.stamp"
+
+  "common_lpf.mod"
+  "COMMON_LPF.mod"
+  "CMakeFiles/letkf020.m01.dir/common_lpf.mod.stamp"
+
+  "common_mpi.mod"
+  "COMMON_MPI.mod"
+  "CMakeFiles/letkf020.m01.dir/common_mpi.mod.stamp"
+
+  "common_mpi_speedy.mod"
+  "COMMON_MPI_SPEEDY.mod"
+  "CMakeFiles/letkf020.m01.dir/common_mpi_speedy.mod.stamp"
+
+  "common_mtx.mod"
+  "COMMON_MTX.mod"
+  "CMakeFiles/letkf020.m01.dir/common_mtx.mod.stamp"
+
+  "common_obs_speedy.mod"
+  "COMMON_OBS_SPEEDY.mod"
+  "CMakeFiles/letkf020.m01.dir/common_obs_speedy.mod.stamp"
+
+  "common_speedy.mod"
+  "COMMON_SPEEDY.mod"
+  "CMakeFiles/letkf020.m01.dir/common_speedy.mod.stamp"
+
+  "common_time.mod"
+  "COMMON_TIME.mod"
+  "CMakeFiles/letkf020.m01.dir/common_time.mod.stamp"
+
+  "interpolate.mod"
+  "INTERPOLATE.mod"
+  "CMakeFiles/letkf020.m01.dir/interpolate.mod.stamp"
+
+  "letkf_obs.mod"
+  "LETKF_OBS.mod"
+  "CMakeFiles/letkf020.m01.dir/letkf_obs.mod.stamp"
+
+  "letkf_tools.mod"
+  "LETKF_TOOLS.mod"
+  "CMakeFiles/letkf020.m01.dir/letkf_tools.mod.stamp"
+
+  "mod_sfmt.mod"
+  "MOD_SFMT.mod"
+  "CMakeFiles/letkf020.m01.dir/mod_sfmt.mod.stamp"
+  )
